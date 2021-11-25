27b12e29d50489fcc150d043f5c1f9a86b6d074e
Vishwas-75 <vishwashnvishu75@gamil.com>
2021-11-25 22:22:20 +0530

    add user auth routes

--- server/index.js
+++ server/index.js
@@ -2,22 +2,22 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import airlineRoutes from './routes/airline.js'
+import userRoutes from './routes/user.js'
 
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true}))
 app.use(cors())
 app.use('/airlines', airlineRoutes)
+app.use('/user', userRoutes)
 
 
-const CONNECTION_URL = 'mongodb+srv://Vishwas-review:vishwasnkatte@cluster0.xwtfz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
-
-mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
         .then(()=>app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
         .catch((error)=> console.log(error.message));
-
-// mongoose.set('useFindAndModify', false);
