13d276056d800bf810dca6bb40fc78601ec9ea34
lokeshg-fsd <lokesh.jejappa@gmail.com>
2022-07-09 17:11:12 +0530

    Add Prettier Configuration and add prettire script to package.json

--- server/config/variables.ts
+++ server/config/variables.ts
@@ -1,6 +1,8 @@
-import { config } from "dotenv";
+import { config } from 'dotenv'
 
-config();
+config()
 
-export const PORT = process.env.PORT || 5000;
-export const DB_URL = process.env.CONNECTION_URL as string;
+export const PORT = process.env.PORT || 5000
+export const DB_URL = process.env.CONNECTION_URL as string
+export const MAIL_TRANSPORTER_EMAIL = process.env.NODE_MIALER_EMAIL as string
+export const MAIL_TRANSPORTER_PASS = process.env.NODE_MIALER_EMAIL_PASS as string
