11c9296554f3e9ea44df3aa7d3da986c88b5b9de
lokeshg-fsd <lokesh.jejappa@gmail.com>
2022-07-09 22:48:50 +0530

    Update Mail transporter Configuration

--- server/config/variables.ts
+++ server/config/variables.ts
@@ -7,3 +7,6 @@ export const DB_URL = process.env.CONNECTION_URL as string
 export const MAIL_TRANSPORTER_EMAIL = process.env.NODE_MAILER_EMAIL as string
 export const MAIL_TRANSPORTER_PASS = process.env.NODE_MAILER_EMAIL_PASS as string
 export const JWT_SECRET = process.env.JWT_SECRET as string
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string
+export const GOOGLE_REFRESH_TOKEN = process.env.GOOGLE_REFRESH_TOKEN as string
